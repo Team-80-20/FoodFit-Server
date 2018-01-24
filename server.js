564324712141e547aec96e1a7902d58a0e7c8e14
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-23 21:04:14 -0800

    updated query

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ const app = express()
 
 app.use(cors())
 
-const __API_URL__ = ''
-
 app.get("/search", (req, res) =>{
     const food = req.query.food
     const minCal = req.query.minCal
     const maxCal = req.query.maxCal
     const healthLabel = req.query.healthLabel
-    const url = `https://api.edamam.com/search?q=${food}&app_id=20bfab81&app_key=56091429890ff1ac97ff6d90ffe5e9ba&from=0&to=8&calories=gte%20${minCal},%20lte%20${maxCal}${healthLabel}`
+    const url = `https://api.edamam.com/search?q=${food}&app_id=20bfab81&app_key=56091429890ff1ac97ff6d90ffe5e9ba&from=0&to=8&calories=gte%20${minCal},%20lte%20${maxCal}&health=alcohol-free${healthLabel}`
     superAgent.get(url)
     .then(function (result) {
         console.log(url)
